9ff55aabe21c8176ffda0ed912e2086ada107bc5
GK029077 <garrett.kliewer@cerner.com>
2015-02-15 13:06:17 -0600

    removed alert box

--- client/activitySelection/activitySelection.js
+++ client/activitySelection/activitySelection.js
@@ -3,7 +3,6 @@ Games = new Mongo.Collection('games');
 Template.activitySelection.events({
   'click button': function (event) {
     Games.insert({activity: event.target.name, createdAt: new Date()});
-    alert('Finding ' + event.target.name + '!');
   }
 });
 
